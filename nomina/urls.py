868ab25781ae6bb489d00f4a8c2fc537f1977da4
thehunter101 <thehuntyer10121@gmail.com>
2025-05-14 04:07:37 -0500

    Listados atomic 100%

--- nomina/urls.py
+++ nomina/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import Inicio, Lista
+from .views import *
 urlpatterns = [
     path("",Inicio, name="Inicio"),
-    path("listado/",Lista, name="Lista"),
+    path("listado/",Empleados, name="Lista"),
+    path("departamento_listado/",Departamentos, name='ListadoDepartamento'),
+    path("cargo_listado/",Cargos, name='ListadoCargo'),
+    path("contrato_listado/",Contrato, name='ListadoContrato'),
+    path("nominas_listado/",Nominas, name='ListadoNominas'),
 ]
--- nomina/views.py
+++ nomina/views.py
@@ -1,14 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from nomina.models import Empleado
+from nomina.models import Cargo, Departamento, Empleado, Rol, TipoContrato
 
 # Create your views here.
 def Inicio(request):
     return render(request,'Inicio.jinja')
 
-def Lista(request):
+def Empleados(request):
     empleados = Empleado.objects.all()
-    return render(request, 'lista_nominas.jinja',{"empleados":empleados})
+    empleados_dict = [{"Nombre":emp.nombre, "Cedula":emp.cedula,"Direccion":emp.direccion,"Sexo":emp.sexo,"Sueldo":emp.sueldo,"Cargo":emp.cargo.descripcion,"Departamento":emp.departamento.descripcion,"Contrato":emp.tipo_contrato.descripcion} for emp in empleados]
+    return render(request, 'lista_nominas.jinja',{"table_body":empleados_dict})
 
-# Lolis
+def Departamentos(request):
+    departamentos = Departamento.objects.all()
+    departamentos_dict = [{
+        "id":dep.pk,"Descripcion":dep.descripcion
+    } for dep in departamentos]
+    return render(request,'departamentos.jinja',{"table_body":departamentos_dict})
+
+def Cargos(request):
+    cargos = Cargo.objects.all()
+    cargos_dict = [{
+        "id":dep.pk,"Descripcion":dep.descripcion
+    } for dep in cargos]
+    return render(request,'cargos.jinja',{"table_body":cargos_dict})
+
+def Contrato(request):
+    contratos = TipoContrato.objects.all()
+    contratos_dict = [{
+        "id":dep.pk,"Descripcion":dep.descripcion
+    } for dep in contratos]
+    return render(request,'contrato.jinja',{"table_body":contratos_dict})
+
+def Nominas(request):
+    nominas = Rol.objects.all()
+    nominas_dict = [{
+        "Empleado":rol.empleado.nombre, "Anio Mes":rol.aniomes, "sueldo":rol.sueldo,
+        "Horas Extra":rol.horas_extra,"Bono":rol.bono,"Iess":rol.iess,"Total":rol.tot_ing,
+        "Descuento":rol.tot_des, "Neto":rol.neto
+    } for rol in nominas]
+    return render(request,'nominas.jinja',{"table_body":nominas_dict})
