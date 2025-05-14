22b8c209caa225bb06c71a6c7da6ceda1a833812
thehunter101 <thehuntyer10121@gmail.com>
2025-05-14 01:16:57 -0500

    Inicio terminado

--- nomina/views.py
+++ nomina/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from nomina.models import Empleado
+
 # Create your views here.
 def Inicio(request):
     return render(request,'Inicio.jinja')
 
 def Lista(request):
-    return render(request, 'lista_nominas.jinja')
+    empleados = Empleado.objects.all()
+    return render(request, 'lista_nominas.jinja',{"empleados":empleados})
