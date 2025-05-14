85673388b4960c3a0e5d86583cc267cf3a074288
JeanVivanco <locomrbream88@gmail.com>
2025-05-14 03:43:55 -0500

    por ahora bien el login

--- nomina/urls.py
+++ nomina/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Inicio, Lista
+from .views import Inicio, Lista,Secion
 urlpatterns = [
     path("",Inicio, name="Inicio"),
     path("listado/",Lista, name="Lista"),
+    path("Secion/",Secion,name="Secion")
 ]
--- nomina/views.py
+++ nomina/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from nomina.models import Empleado
 
 # Create your views here.
@@ -10,5 +11,30 @@ def Inicio(request):
 def Lista(request):
     empleados = Empleado.objects.all()
     return render(request, 'lista_nominas.jinja',{"empleados":empleados})
+def Secion(request):
+    if request.method == "GET":
+        return render(request,'singup.jinja',{
+        'form':UserCreationForm
+        })
+    else:
+        if request.POST['password1']== request.POST['password2']:
+            try:
+                # Resgistra usuario
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado bien')
+
+
+            except:
+                return render(request,'singup.jinja',{
+                    'form':UserCreationForm,
+                'error':'El usuario ya existe'})
+
+        return render(request,'singup.jinja',{
+                'form':UserCreationForm,
+                'error':'Las contrase;as no coinciden'
+                })
+
+
 
 # Lolis
